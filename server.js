a62347daacba58a3256c04580d035dce49fe692a
tworedz <nurik99kg@gmail.com>
2020-03-11 21:00:02 +0600

    add logo

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require('express');
 const path = require('path');
 const app = new express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 
 app.use(express.static('static'));
 
 
 app.get('/', function(request, response){
-    responce.send("Helllo!");
+    response.send("Hello");
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port 4000!');
+  console.log('Example app listening on port ' + PORT);
 })
