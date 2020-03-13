b7df7dab27d13dd725ad801d44c0ff0abed5712c
tworedz <nurik99kg@gmail.com>
2020-03-13 16:12:33 +0600

    Add RESTful api

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
+
+
 const app = new express();
 
 const PORT = process.env.PORT || 5000
@@ -8,11 +11,39 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('static'));
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 
-app.get('/', function(request, response){
-    response.send("Hello");
+app.post('/debug', (req, res) => {
+    let body = req.body;
+    if (body.latitude && body.longitude && body.data){
+      res.status(200).json({
+        message: "Succesfully get data",
+        data: body
+      });
+    }else{
+      res.status(400).json({
+        message: "Error. Bad request"
+      })
+    }
 });
 
-app.listen(PORT, function () {
+
+app.post('/to_map', (req, res) => {
+    let body = req.body;
+    if (body.latitude && body.longitude && body.data){
+
+      res.status(200).json({
+        message: "Succesfully get data"
+      });
+    }else{
+      res.status(400).json({
+        message: "Error. Bad request"
+      })
+    }
+});
+
+
+app.listen(PORT, () => {
   console.log('Example app listening on port ' + PORT);
 })
--- static/script.js
+++ static/script.js
@@ -1,8 +1,10 @@
 var map;
 
-    DG.then(function () {
-        map = DG.map('map', {
-            center: [54.98, 82.89],
-            zoom: 15
-        });
+DG.then(function () {
+    map = DG.map('map', {
+        center: [84.98, 82.89],
+        zoom: 15
     });
+
+    DG.marker([54.95, 82.89]).addTo(map);
+});
